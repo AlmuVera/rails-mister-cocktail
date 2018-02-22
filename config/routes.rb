fa358a2380180ae2ca2535be4777c252f88d42b7
Almudena Vera <almudena.vigara@gmail.com>
2018-02-22 18:58:30 +0100

    added controllers cocktails, ingredients and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'doses/create'
+
+  get 'doses/destroy'
+
+  get 'ingredients/show'
+
+ resources :cocktails, only: [:index, :show, :new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "cocktails#home"
+  root to: "cocktails#index"
 end
